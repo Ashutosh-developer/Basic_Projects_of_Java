16428e89485276ab36a0288441e0ccabedcfd952
Ashutosh <ashutosh.developer01@gmail.com>
2022-07-15 13:16:37 +0530

    Projects of java

---
+++ Calculator by switch_case/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Calculator{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println("For Addition press 1:");
+		System.out.println("For Substraction press 2:");
+		System.out.println("For Division press 3:");
+		System.out.println("For Multiplication press 4:");
+
+
+		int c = sc.nextInt();
+
+
+		switch(c){
+		case 1: System.out.println(a+b);
+			break;
+		case 2: System.out.println(a-b);
+			break;
+		case 3: System.out.println(a/b);
+			break;
+		case 4: System.out.println(a*b);
+			break;
+		}
+		// System.out.println(result);
+	}
+}
+	
---
+++ Pong Game/GameFrame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    
+    GamePanel panel = new GamePanel();
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
