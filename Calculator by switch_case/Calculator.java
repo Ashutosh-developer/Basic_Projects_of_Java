000c9695684fdd316bd328407e2aa6f76757be77
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-10 14:46:55 +0530

    Calculator by Switch case for simple calculation it can do operation on 2 numbers only.....

--- Calculator by switch_case/Calculator.java
+++ Calculator by switch_case/Calculator.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Calculator{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 
@@ -15,14 +16,16 @@ public class Calculator{
 
 
 		switch(c){
-		case 1: System.out.println(a+b);
-			break;
-		case 2: System.out.println(a-b);
-			break;
-		case 3: System.out.println(a/b);
-			break;
-		case 4: System.out.println(a*b);
-			break;
+			case 1: System.out.println(a+b);
+				break;
+			case 2: System.out.println(a-b);
+				break;
+			case 3: System.out.println(a/b);
+				break;
+			case 4: System.out.println(a*b);
+				break;
+			default:
+				throw new IllegalStateException("Unexpected value: " + c);
 		}
 		// System.out.println(result);
 	}
